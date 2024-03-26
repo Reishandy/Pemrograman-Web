901b7b9827d65152a6cab62dd5d2a8b37c7e937d
M. Akbar Reishandy <isthisruxury@gmail.com>
2024-03-26 08:53:57 +0700

    Tugas 1 + 2

---
+++ main/tugas_2.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=chrome">
+    <title>Contoh 02</title>
+</head>
+<body style="background-color: pink; color: black">
+
+<p>
+    Ini adalah contoh penggunaan formating TAG dalam HTML <br>
+    masing-masing TAG memiliki atribut masing-masing<br>
+    ini adalah paragraf <br>
+</p>
+
+<span style="color: blue; ">
+    Ini juga termasuk contoh penggunaan formating TAG<br>
+</span>
+
+<h1>
+    <marquee width="100%" bgcolor="blue">Ini juga salah satu penggunaan Tag</marquee>
+</h1>
+
+<h1>HTML Image</h1>
+<h2>HTML Image</h2>
+<h6>HTML Image</h6>
+
+<img src="../assets/10.jpg" alt="Michiru" width="500" height="333">
+<br>
+<img src="../assets/20.png" alt="Uniwara" width="500" height="333">
+
+<h1>HTML Links</h1>
+
+<p>
+    <a href="https://www.w3schools.com/">Visit W3Schools.com </a>
+</p>
+
+<p>
+    <a href="https://www.uniwara.ac.id/">Visit UNIVERSITAS PGRI WIRANEGARA </a>
+</p>
+
+<p>
+    <a href="https://www.reishdany.rf.gd/">Visit My Website</a>
+</p>
+
+<?php  // Opening penulisan script php
+
+// Script dibawah ini adalah contoh deklarasi variabel
+$nim = "12170829";
+$nama = "Bima Bintang Galaxy";
+$kelas = "12.1A.01";
+
+// Script dibawah ini digunakan sebagai output
+echo "Nim Saya = $nim<br>";
+echo "Nama Saya = $nama<br>";
+echo "Kelas Saya = $kelas<br>";
+
+// Script dibawah ini adalah closing penulisan script php
+?>
+
+</body>
+</html>
